3a933c83b5bda52406be57a56d1283227f1776ef
DiMs8P <dota.dimabog@mail.ru>
2024-12-23 20:55:28 +0700

    added some funcitons

--- CourseProjectDima/CourseProjectDima/Config.cs
+++ CourseProjectDima/CourseProjectDima/Config.cs
@@ -9,6 +9,8 @@ public static class Config
     {
         (p, t) => Math.Pow(p.R, 1) + Math.Pow(p.Z, 1), // f = r + z
         (p, t) => Math.Pow(p.R, 2) + Math.Pow(p.Z, 2), // f = r^2 + z^2
+        (p, t) => Math.Pow(p.R, 3) + Math.Pow(p.Z, 3), // f = r^3 + z^3
+        (p, t) => Math.Pow(p.R - 6, 3) + Math.Pow(p.Z - 6, 3), // f = (r - 6)^3 + (z - 6)^3
         (p, t) => Math.Exp(p.R) + Math.Exp(p.Z), // f = exp(r) + exp(z)
         (p, t) => Math.Sin(p.R) * Math.Cos(p.Z)
     };
@@ -17,6 +19,8 @@ public static class Config
     {
         (p, t) => -1 / p.R, // f = r + z
         (p, t) => -6, // f = r^2 + z^2
+        (p, t) => -9 * Math.Pow(p.R, 1) -6 * Math.Pow(p.Z, 1), // f = r^3 + z^3
+        (p, t) => -1 / p.R * (3 * Math.Pow(p.R - 6, 2) + 6 * p.R * (p.R - 6)) - 6 *(p.Z - 6), // f = (r - 6)^3 + (z - 6)^3
         (p, t) => -Math.Exp(p.R) * (1 + p.R) / p.R - Math.Exp(p.Z), // f = exp(r) + exp(z)
         (p, t) => Math.Cos(p.Z) * (p.R * Math.Sin(p.R) - Math.Cos(p.R)) / p.R + Math.Sin(p.R) * Math.Cos(p.Z),
     };
@@ -25,11 +29,13 @@ public static class Config
     {
         "r+z",
         "r^2+z^2",
+        "r^3+z^3",
+        "(r - 6)^3 + (z - 6)^3",
         "exp(r) + exp(z)",
         "sin(r)cos(z)"
     };
 
-    private const int FuncIndex = 1;
+    private const int FuncIndex = 3;
 
     public static Func<Node2D, double, double> u = (p, t) => Us[FuncIndex](p, t);
     public static Func<Node2D, double, double> f = (p, t) => Fs[FuncIndex](p, t);
